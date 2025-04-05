c7b9e60ddc4eca629a6dfbaa9f64b9bba613b141
anggarenn <57941599+anggarenn@users.noreply.github.com>
2025-04-06 04:49:21 +0700

    Update label103.py

--- label103.py
+++ label103.py
@@ -95,7 +95,7 @@ if input_option == "Input Manual":
             daftar_nama = daftar_nama_input.splitlines()
             output_file = "label_undangan.docx"
             result = create_label_docx(daftar_nama, template_awal, template_akhir, output_file)
-            st.success(f"✅ Label berhasil dibuat! File disimpan di: {output_file}")
+            st.success(f"✅ Label berhasil dibuat! File disimpan dengan nama file: {output_file}")
 
             # Offer untuk download file
             with open(output_file, "rb") as f:
